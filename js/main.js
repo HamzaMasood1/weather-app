d44bc96474ea31fa7d0ee98670bcfbacf7e61111
HamzaMasood1 <hamzamasood183@gmail.com>
2020-11-16 23:36:24 +0000

    successfully accessed api

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@ var cObj;
 var fObj;
 
 // GET THE CONDITIONS
-weatherConditions.open('', '', true);
+weatherConditions.open('GET', 'http://api.openweathermap.org/data/2.5/weather?zip=84653,US&appid=efd2ac236bbfe9ce45441d423cbafaf7', true);
 weatherConditions.responseType = 'text';
 weatherConditions.send(null);
 
-weatherConditions.onload = function() {
-    if (weatherConditions.status === 200){
-        cObj = JSON.parse(weatherConditions.responseText); 
+weatherConditions.onload = function () {
+    if (weatherConditions.status === 200) {
+        cObj = JSON.parse(weatherConditions.responseText);
         console.log(cObj);
 
 
@@ -30,16 +30,16 @@ weatherConditions.onload = function() {
 
 
 // GET THE FORECARST
-weatherForecast.open('', '', true);
-weatherForecast.responseType = 'text'; 
-weatherForecast.send();
+//weatherForecast.open('', '', true);
+weatherForecast.responseType = 'text';
+//weatherForecast.send();
 
-weatherForecast.onload = function() {
-if (weatherForecast.status === 200){
-	fObj = JSON.parse(weatherForecast.responseText);
-	console.log(fObj);
-	
-} //end if
+weatherForecast.onload = function () {
+    if (weatherForecast.status === 200) {
+        fObj = JSON.parse(weatherForecast.responseText);
+        console.log(fObj);
+
+    } //end if
 }; //end function
 
 
